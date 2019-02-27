561b35ce9694a72f9650e73f794a8dd816037c1d
Finnian <finnian2010@hotmail.com>
2019-02-27 19:09:24 +0000

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# TaskDrivenDevelopment-Task3" 
---
+++ src/cm/RateTest.java
@@ -0,0 +1,286 @@
+package cm;
+
+import static org.junit.Assert.assertEquals;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class RateTest 
+{	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void reducedPeriodsNull()
+	{
+		Period[] newArray = {new Period(13, 16),new Period(17, 23)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Period[] array = {new Period(1, 6),new Period(7, 12)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Rate theRate = new Rate(CarParkKind.STAFF, BigDecimal.valueOf(10), BigDecimal.valueOf(5), null, theNormalPeriods);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void normalPeriodsNull()
+	{
+		Period[] newArray = {new Period(13, 16),new Period(17, 23)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Period[] array = {new Period(1, 6),new Period(7, 12)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Rate theRate = new Rate(CarParkKind.STAFF, BigDecimal.valueOf(10), BigDecimal.valueOf(5),theReducedPeriods, null);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void normalRateNull()
+	{
+		Period[] newArray = {new Period(13, 16),new Period(17, 23)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Period[] array = {new Period(1, 6),new Period(7, 12)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Rate theRate = new Rate(CarParkKind.STAFF, null, BigDecimal.valueOf(10), theReducedPeriods, theNormalPeriods);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void ReducedRateNull()
+	{
+		Period[] newArray = {new Period(13, 16),new Period(17, 23)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Period[] array = {new Period(1, 6),new Period(7, 12)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Rate theRate = new Rate(CarParkKind.STAFF, BigDecimal.valueOf(10), null, theReducedPeriods, theNormalPeriods);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	
+	public void testReducedOverlap()
+	{	
+		Period[] array = {new Period(1, 6),new Period(5, 9)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Period[] newArray = {new Period(7, 12),new Period(13, 16)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Rate theRate = new Rate(CarParkKind.STAFF, BigDecimal.valueOf(5), BigDecimal.valueOf(2), theReducedPeriods, theNormalPeriods);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testNormalOverlap()
+	{	
+		Period[] array = {new Period(1, 6),new Period(7, 12)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Period[] newArray = {new Period(13, 17),new Period(14, 19)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Rate theRate = new Rate(CarParkKind.STAFF, BigDecimal.valueOf(5), BigDecimal.valueOf(2), theReducedPeriods, theNormalPeriods);
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testPeriodsOverlapEachother()
+	{	
+		Period[] array = {new Period(1, 6),new Period(7, 12)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Period[] newArray = {new Period(5, 13),new Period(16, 19)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Rate theRate = new Rate(CarParkKind.STAFF, BigDecimal.valueOf(5), BigDecimal.valueOf(2), theReducedPeriods, theNormalPeriods);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testNormalRateLessThanZero()
+	{	
+		Period[] array = {new Period(1, 6),new Period(7, 12)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Period[] newArray = {new Period(13, 17),new Period(18, 20)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Rate theRate = new Rate(CarParkKind.STAFF, BigDecimal.valueOf(-1), BigDecimal.valueOf(0), theReducedPeriods, theNormalPeriods);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testNomalRateLessThanZeroNonInteger()
+	{	
+		Period[] array = {new Period(1, 6),new Period(7, 12)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Period[] newArray = {new Period(13, 17),new Period(18, 20)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Rate theRate = new Rate(CarParkKind.STAFF, BigDecimal.valueOf(-0.1), BigDecimal.valueOf(0), theReducedPeriods, theNormalPeriods);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testReducedRateLessThanZero()
+	{	
+		Period[] array = {new Period(1, 6),new Period(7, 12)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Period[] newArray = {new Period(13, 17),new Period(18, 20)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Rate theRate = new Rate(CarParkKind.STAFF, BigDecimal.valueOf(5), BigDecimal.valueOf(-1), theReducedPeriods, theNormalPeriods);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testNullCarkParkKind()
+	{	
+		Period[] array = {new Period(1, 6),new Period(7, 12)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Period[] newArray = {new Period(13, 17),new Period(18, 20)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Rate theRate = new Rate(null, BigDecimal.valueOf(5), BigDecimal.valueOf(-0.1), theReducedPeriods, theNormalPeriods);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testReducedRateGreaterThanNormal()
+	{	
+		Period[] array = {new Period(1, 6),new Period(7, 12)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Period[] newArray = {new Period(13, 17),new Period(18, 20)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Rate theRate = new Rate(CarParkKind.STAFF, BigDecimal.valueOf(5), BigDecimal.valueOf(10), theReducedPeriods, theNormalPeriods);
+	}
+	
+	@Test
+	public void testNormalRateGreaterThanReduced()
+	{	
+		Period[] array = {new Period(1, 6),new Period(7, 12)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Period[] newArray = {new Period(13, 17),new Period(18, 20)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Rate theRate = new Rate(CarParkKind.STAFF, BigDecimal.valueOf(20), BigDecimal.valueOf(10), theReducedPeriods, theNormalPeriods);
+	}
+	
+	@Test
+	public void testNoOverlap()
+	{	
+		Period[] array = {new Period(1, 6),new Period(7, 12)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Period[] newArray = {new Period(13, 17),new Period(18, 20)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Rate theRate = new Rate(CarParkKind.STAFF, BigDecimal.valueOf(20), BigDecimal.valueOf(10), theReducedPeriods, theNormalPeriods);
+	}
+	
+	@Test
+	public void testCalculateNormalPeriod()
+	{
+		Period[] newArray = {new Period(13, 17),new Period(19, 23)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Period[] array = {new Period(1, 6),new Period(7, 12)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Rate theRate = new Rate(CarParkKind.STAFF, BigDecimal.valueOf(5), BigDecimal.valueOf(2), theReducedPeriods, theNormalPeriods);
+		Period chargePeriod = new Period(14, 16);
+		assertEquals(BigDecimal.valueOf(10), theRate.calculate(chargePeriod));
+	}
+	
+	@Test
+	public void testCalculateFreePeriod()
+	{
+		Period[] array = {new Period(1, 6),new Period(7, 12)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Period[] newArray = {new Period(13, 16),new Period(19, 23)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Rate theRate = new Rate(CarParkKind.STAFF, BigDecimal.valueOf(5), BigDecimal.valueOf(2), theReducedPeriods, theNormalPeriods);
+		Period chargePeriod = new Period(17, 18);
+		assertEquals(BigDecimal.valueOf(0), theRate.calculate(chargePeriod));
+	}
+	
+	@Test
+	public void testCalculateReducedPeriod()
+	{
+		Period[] array = {new Period(1, 6),new Period(9, 12)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Period[] newArray = {new Period(13, 16),new Period(19, 23)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Rate theRate = new Rate(CarParkKind.STAFF, BigDecimal.valueOf(5), BigDecimal.valueOf(2), theReducedPeriods, theNormalPeriods);
+		Period chargePeriod = new Period(2, 6);
+		assertEquals(BigDecimal.valueOf(8), theRate.calculate(chargePeriod));
+	}
+	
+	@Test
+	public void testCalculateBothPeriods()
+	{
+		Period[] array = {new Period(1, 6),new Period(9, 12)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Period[] newArray = {new Period(13, 16),new Period(19, 23)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Rate theRate = new Rate(CarParkKind.STAFF, BigDecimal.valueOf(5), BigDecimal.valueOf(2), theReducedPeriods, theNormalPeriods);
+		Period chargePeriod = new Period(10, 14);
+		assertEquals(BigDecimal.valueOf(9), theRate.calculate(chargePeriod));
+	}
+	
+	@Test
+	public void testCalculateFreeToNormalPeriod()
+	{
+		Period[] array = {new Period(1, 6),new Period(9, 12)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Period[] newArray = {new Period(13, 15),new Period(19, 23)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Rate theRate = new Rate(CarParkKind.STAFF, BigDecimal.valueOf(5), BigDecimal.valueOf(2), theReducedPeriods, theNormalPeriods);
+		Period chargePeriod = new Period(16, 23);
+		assertEquals(BigDecimal.valueOf(20), theRate.calculate(chargePeriod));
+	}
+	
+	@Test
+	public void testCalculateFreeToReducedPeriod()
+	{
+		Period[] array = {new Period(1, 6),new Period(9, 12)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Period[] newArray = {new Period(13, 15),new Period(19, 23)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Rate theRate = new Rate(CarParkKind.STAFF, BigDecimal.valueOf(5), BigDecimal.valueOf(2), theReducedPeriods, theNormalPeriods);
+		Period chargePeriod = new Period(7, 12);
+		assertEquals(BigDecimal.valueOf(6), theRate.calculate(chargePeriod));
+	}
+	
+	@Test
+	public void testCalculateAllThreePeriods()
+	{
+		Period[] array = {new Period(1, 6),new Period(9, 12)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Period[] newArray = {new Period(13, 15),new Period(19, 23)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Rate theRate = new Rate(CarParkKind.STAFF, BigDecimal.valueOf(5), BigDecimal.valueOf(2), theReducedPeriods, theNormalPeriods);
+		Period chargePeriod = new Period(7, 15);
+		assertEquals(BigDecimal.valueOf(16), theRate.calculate(chargePeriod));
+	}
+
+	@Test
+	public void testCalculateVisitor1()
+	{
+		Period[] array = {new Period(1, 6),new Period(9, 12)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Period[] newArray = {new Period(13, 15),new Period(19, 23)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Rate theRate = new Rate(CarParkKind.VISITOR, BigDecimal.valueOf(5), BigDecimal.valueOf(2), theReducedPeriods, theNormalPeriods);
+		Period chargePeriod = new Period(13, 15);
+		assertEquals(BigDecimal.valueOf(1), theRate.calculate(chargePeriod));
+	}
+	
+	@Test
+	public void testCalculateVisitor2()
+	{
+		Period[] array = {new Period(1, 6),new Period(9, 13)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Period[] newArray = {new Period(13, 15),new Period(19, 23)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Rate theRate = new Rate(CarParkKind.VISITOR, BigDecimal.valueOf(5), BigDecimal.valueOf(2), theReducedPeriods, theNormalPeriods);
+		Period chargePeriod = new Period(9, 15);
+		assertEquals(BigDecimal.valueOf(8), theRate.calculate(chargePeriod));
+	}
+	
+	@Test
+	public void testCalculateVisitor3()
+	{
+		Period[] array = {new Period(1, 6),new Period(9, 13)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Period[] newArray = {new Period(13, 15),new Period(19, 23)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Rate theRate = new Rate(CarParkKind.VISITOR, BigDecimal.valueOf(5), BigDecimal.valueOf(2), theReducedPeriods, theNormalPeriods);
+		Period chargePeriod = new Period(7, 20);
+		assertEquals(BigDecimal.valueOf(7.5), theRate.calculate(chargePeriod));
+	}
+	
+	@Test
+	public void testCalculateVisitor4()
+	{
+		Period[] array = {new Period(1, 6),new Period(9, 13)};
+		ArrayList<Period> theReducedPeriods = new ArrayList<Period>(Arrays.asList(array));
+		Period[] newArray = {new Period(13, 15),new Period(19, 23)};
+		ArrayList<Period> theNormalPeriods = new ArrayList<Period>(Arrays.asList(newArray));
+		Rate theRate = new Rate(CarParkKind.VISITOR, BigDecimal.valueOf(5), BigDecimal.valueOf(2), theReducedPeriods, theNormalPeriods);
+		Period chargePeriod = new Period(7, 20);
+		assertEquals(BigDecimal.valueOf(7.5), theRate.calculate(chargePeriod));
+	}
+}
